1e12df23fe06b652bf7938a75d8823368e6c3b56
Marcus_stevelind <marcus.stevelind@gmail.com>
2016-04-22 10:04:18 +0200

    Scan test ej helt komplett och början av en meny

--- main/__init__.py
+++ main/__init__.py
@@ -4,6 +4,45 @@
 #imports
 import socket
 from bluepymaster.bluepy.btle import Scanner
+response = True
+responseNumber = 0
+while response:
+    print ("""
+    1.
+    2.
+    3.
+    4.
+    5.
+    6.
+    7.
+    8.
+    9.
+    10. Quit""")
+    responseNumber=input("What would you like to do")
+    if responseNumber=="1" :
+        print ("hubba bubba")
+    elif responseNumber=="2" :
+        print ("hubba bubba2")
+    elif responseNumber=="3" :
+        print ("hubba bubba3")
+    elif responseNumber=="4" :
+        print ("hubba bubba4")
+    elif responseNumber=="5" :
+        print ("hubba bubba5")
+    elif responseNumber=="6" :
+        print ("hubba bubba6")
+    elif responseNumber=="7" :
+        print ("hubba bubba7")
+    elif responseNumber=="8" :
+        print ("hubba bubba8")
+    elif responseNumber=="9" :
+        print ("hubba bubba9")
+    elif responseNumber=="10" :
+        print ("Bye")
+    
+    
+
+
 class Gateway:
     """Main class for communication between user program and the system""" 
     def __init__(self,dbid=None):
