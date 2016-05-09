4dec1b8030ad28d6de18973876e3b84e9d5285a0
kallee <kallewowspelaren@gmail.com>
2016-05-09 15:46:41 +0200

    test

--- main/__init__.py
+++ main/__init__.py
@@ -196,8 +196,7 @@ def interpretPopInstruction(result,server,gateway):
         instruction = result.split(",")
         gateway.updatePopulation(instruction[2:5],instruction[1])
         print("instruction handled!")
-    time.sleep(0.1)
-    kademliaPopInstructionListener(server, gateway)
+    server.addCallback(kademliaPopInstructionListener,server,gateway)
 
 def interpretGroupsInstruction(result,server,gateway):
     if result == None or result == "0" or result.split(",")[5] in  HANDLED_INSTRUCTIONS:
