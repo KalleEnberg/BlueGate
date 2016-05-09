4fb5a17c1e480070ed7dc8c249e8013ff20c83c2
kallee <kallewowspelaren@gmail.com>
2016-05-09 15:34:28 +0200

    test

--- main/__init__.py
+++ main/__init__.py
@@ -191,13 +191,13 @@ def createGroupsInstruction(groupids,uuid,major,minor,soft_reboot):
     return res[:-1] + "," + uuid + "," + major + "," + minor + "," + soft_reboot + "," + str(time.time() * 1000)
 def interpretPopInstruction(result,server,gateway):
 
-    if result == None or result == "0" or result.split(",")[0] != GATEWAY_ID or result.split(",")[6] in  HANDLED_INSTRUCTIONS:
-        print "vi kommer hit"
-        pass
-    else:        
-        instruction = result.split(",")
-        gateway.updatePopulation(instruction[2:5],instruction[1])
-        print("instruction handled!")
+#     if result == None or result == "0" or result.split(",")[0] != GATEWAY_ID or result.split(",")[6] in  HANDLED_INSTRUCTIONS:
+#         print "vi kommer hit"
+#         pass
+#     else:        
+#         instruction = result.split(",")
+#         gateway.updatePopulation(instruction[2:5],instruction[1])
+#         print("instruction handled!")
     kademliaPopInstructionListener(server, gateway)
 
 def interpretGroupsInstruction(result,server,gateway):
