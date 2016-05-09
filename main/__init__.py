736d8973eae5d2ada35d882246b14c9a73a47102
kallee <kallewowspelaren@gmail.com>
2016-05-09 16:49:57 +0200

    jag vill inte se 0:or

--- main/__init__.py
+++ main/__init__.py
@@ -191,7 +191,7 @@ def createGroupsInstruction(groupids,uuid,major,minor,soft_reboot):
         res += groupid + ":"
     return res[:-1] + "," + uuid + "," + major + "," + minor + "," + soft_reboot + "," + str(time.time() * 1000)
 def interpretPopInstruction(result,server,gateway):
-    if result != None or result != 0 or result !="0":
+    if (result != None) or (result != 0) or (result !="0"):
         print(type(result))
         print(result)
 #     if result == None or result == "0" or result.split(",")[0] != GATEWAY_ID or result.split(",")[6] in  HANDLED_INSTRUCTIONS:
