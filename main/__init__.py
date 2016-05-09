6071f347fb7c3fc3b095a54099e753ad7909d7e1
kallee <kallewowspelaren@gmail.com>
2016-05-09 15:16:16 +0200

    fix??

--- main/__init__.py
+++ main/__init__.py
@@ -225,8 +225,8 @@ def kademliaGroupInstructionListener(server,gateway):
                     
 def main(arg,server,gateway,first=False):
     if first:
-        #thread.start_new_thread(kademliaPopInstructionListener, (server,gateway))
-        thread.start_new_thread(kademliaGroupInstructionListener, (server,gateway))
+        thread.start_new_thread(kademliaPopInstructionListener, (server,gateway))
+        #thread.start_new_thread(kademliaGroupInstructionListener, (server,gateway))
     g = gateway
     response = True
     responseNumber = 0
@@ -421,7 +421,7 @@ def main(arg,server,gateway,first=False):
         elif responseNumber=="25" :
             print ("Bye!")
             response = False
-            reactor.stop()
+            reactor.stop() #Make flag for killing the threads before this call
     if g.dbconnection:
         g.dbconnection.close()
 #log.startLogging(sys.stdout) #ta bort kommentar for loggning i nod
