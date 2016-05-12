0f20ca053d384ab78a80c45d021c69cc7d134442
kallee <kallewowspelaren@gmail.com>
2016-05-12 16:57:54 +0200

    test

--- main/__init__.py
+++ main/__init__.py
@@ -195,8 +195,8 @@ def interpretPopInstruction(result,gateway):
 #         print("instruction handled!")
 
 def interpretGroupsInstruction(result,server,gateway):
-        print(result)
-    if result = None or result.split(",")[5] in  HANDLED_INSTRUCTIONS:
+    print(result)
+    if result == None or result.split(",")[5] in  HANDLED_INSTRUCTIONS:
         pass
     else:
         instruction = result.split(",")
